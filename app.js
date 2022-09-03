f0ad6b2bef131cd13701899f48661188cf905bc5
molaraiche <molaraiche@molaraiche.ma>
2022-09-03 15:06:18 +0100

    set logic

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ const playerTwoImg = document.querySelector(".PlayerTwoImg");
 startBtn.onclick = () => {
   let shufflePlayerOne = Math.trunc(Math.random() * 3) + 1;
   let shufflePlayerTwo = Math.trunc(Math.random() * 3) + 1;
-  playerOneImg.src = `./images/${shufflePlayerOne}.png`;
-  playerTwoImg.src = `./images/${shufflePlayerTwo}.png`;
+  playerOneImg.src = `${shufflePlayerOne}.png`;
+  playerTwoImg.src = `${shufflePlayerTwo}.png`;
+  console.log(shufflePlayerOne);
+  console.log(shufflePlayerTwo);
+  // Game Logic
+  let paper = 2;
+  let stone = 1;
+  let scissor = 3;
+  if (shufflePlayerOne === shufflePlayerTwo) {
+    console.log("draw");
+  } else if (shufflePlayerOne === 1 && shufflePlayerTwo === 2) {
+    console.log("Player Two win");
+  } else if (shufflePlayerOne === 2 && shufflePlayerTwo === 1) {
+    console.log("player one win");
+  } else if (shufflePlayerOne === 1 && shufflePlayerTwo === 3) {
+    console.log("player one win");
+  } else if (shufflePlayerOne === 3 && shufflePlayerTwo === 1) {
+    console.log("player Two win");
+  } else if (shufflePlayerOne === 2 && shufflePlayerTwo === 3) {
+    console.log("player two win");
+  } else if (shufflePlayerOne === 3 && shufflePlayerTwo === 2) {
+    console.log("player One Win");
+  }
 };
-
-// Game Logic
