a51c28ab9458d8a3503791ea77fc88e75f844a78
molaraiche <molaraiche@molaraiche.ma>
2022-09-03 22:08:02 +0100

    finished

--- app.js
+++ app.js
@@ -1,31 +1,74 @@
 const startBtn = document.querySelector(".startBtn");
+const resetBtn = document.querySelector(".resetBtn");
 const playerOneImg = document.querySelector(".PlayerOneImg");
 const playerTwoImg = document.querySelector(".PlayerTwoImg");
-// Create the Handle Click
+const POR = document.querySelector(".POR");
+const PTR = document.querySelector(".PTR");
+// Create the Handle Click ✔
 startBtn.onclick = () => {
   let shufflePlayerOne = Math.trunc(Math.random() * 3) + 1;
   let shufflePlayerTwo = Math.trunc(Math.random() * 3) + 1;
   playerOneImg.src = `${shufflePlayerOne}.png`;
   playerTwoImg.src = `${shufflePlayerTwo}.png`;
-  console.log(shufflePlayerOne);
-  console.log(shufflePlayerTwo);
-  // Game Logic
+
+  // Game Logic 😁
   let paper = 2;
   let stone = 1;
   let scissor = 3;
   if (shufflePlayerOne === shufflePlayerTwo) {
-    console.log("draw");
+    POR.style.fontWeight = 700;
+    POR.style.color = "#52ad00";
+    POR.textContent = "DRAW 👏";
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "#52ad00";
+    PTR.textContent = "DRAW 👏 ";
   } else if (shufflePlayerOne === 1 && shufflePlayerTwo === 2) {
-    console.log("Player Two win");
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "#52ad00";
+    PTR.textContent = "WINNER 🎉 ";
+    POR.style.fontWeight = 700;
+    POR.style.color = "red";
+    POR.textContent = "LOSER ❌ ";
   } else if (shufflePlayerOne === 2 && shufflePlayerTwo === 1) {
-    console.log("player one win");
+    POR.style.fontWeight = 700;
+    POR.style.color = "#52ad00";
+    POR.textContent = "WINNER  🎉 ";
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "red";
+    PTR.textContent = "LOSER ❌ ";
   } else if (shufflePlayerOne === 1 && shufflePlayerTwo === 3) {
-    console.log("player one win");
+    POR.style.fontWeight = 700;
+    POR.style.color = "#52ad00";
+    POR.textContent = "WINNER  🎉 ";
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "red";
+    PTR.textContent = "LOSER ❌ ";
   } else if (shufflePlayerOne === 3 && shufflePlayerTwo === 1) {
-    console.log("player Two win");
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "#52ad00";
+    PTR.textContent = "WINNER  🎉 ";
+    POR.style.fontWeight = 700;
+    POR.style.color = "red";
+    POR.textContent = "LOSER ❌ ";
   } else if (shufflePlayerOne === 2 && shufflePlayerTwo === 3) {
-    console.log("player two win");
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "#52ad00";
+    PTR.textContent = "WINNER  🎉 ";
+    POR.style.fontWeight = 700;
+    POR.style.color = "red";
+    POR.textContent = "LOSER ❌ ";
   } else if (shufflePlayerOne === 3 && shufflePlayerTwo === 2) {
-    console.log("player One Win");
+    POR.style.fontWeight = 700;
+    POR.style.color = "#52ad00";
+    POR.textContent = "WINNER  🎉 ";
+    PTR.style.fontWeight = 700;
+    PTR.style.color = "red";
+    PTR.textContent = "LOSER ❌ ";
   }
+};
+resetBtn.onclick = () => {
+  POR.textContent = "💭";
+  PTR.textContent = "💭";
+  playerOneImg.src = "1.png";
+  playerTwoImg.src = "1.png";
 };
